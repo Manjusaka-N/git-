64274c5deacb875c757af271b7216cf2a7e01fbd
zhengyunan <yn.zheng@centralsoft.com.cn>
2022-06-19 21:53:56 +0800

    <feat>第一次本地提交

---
+++ src/components/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    xxxx
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.test{
+    // 本地第一次提交白色
+    color: white;
+}
+
+</style>
